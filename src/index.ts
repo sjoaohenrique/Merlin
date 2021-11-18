6ff95be21b6bee18454accaa19918a9b82192e3b
joao <joao.souza@convenia.com.br>
2021-11-17 22:19:31 -0300

    run prettier

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express, { Application } from "express";
+import express, { Application } from 'express';
 
 export default class Merlin {
-    private app: Application;
+  private app: Application;
 
-    constructor () {
-        this.app = express();
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended: true }));
-    }
-    run() {
-        this.app.listen(3000, (): void => console.log("Server start successfuly"))
-    }
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+  run() {
+    this.app.listen(3000);
+  }
 }
